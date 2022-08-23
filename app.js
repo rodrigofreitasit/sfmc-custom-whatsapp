652956646a0dd34395487ff17d6a1db64a43509e
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2022-08-23 18:17:12 -0300

    create server

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 var express = require('express')
 var bodyParser = require("body-parser");
+var http = require('http');
 var router = require('./routes/router')
 var activityRoutes = require('./routes/activityRoutes')
 var app = express()
 var path = require('path')
 
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.urlencoded({
-    extended: false
+app.use(bodyParser.raw({
+    type: 'application/jwt'
 }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname + '/public')));
 
+if ('development' == app.get('env')) {
+    app.use(errorhandler());
+}
+
 app.use('/', router);
-
-
-
 app.use('/journeybuilder/', activityRoutes);
 
-var server = app.listen(3000, function () {
-    console.log('Node server is running..');
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
